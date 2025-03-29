bca7c1d04e512aadb089faa08861a052853fcfe0
WalliCode <wallisonjuniodias@gmail.com>
2025-03-28 22:33:07 -0300

    Operadores Java

---
+++ Operadores/src/Operadores.java
@@ -0,0 +1,20 @@
+public class Operadores {
+    public static void main(String[] args) throws Exception {
+
+        boolean condicao1 = true;
+
+        boolean condicao2 = false;
+
+        if(condicao1 && condicao2) {
+        System.out.println("Os dois são verdadeiros");
+        }
+
+        if(condicao1 || condicao2) {
+            System.out.println("Um dos dois são verdadeiros");
+            }
+
+        System.out.println("FIM");
+
+    }
+
+}
