5e44c3659783637b461b115913f1ffd965a027e2
diaswalli <wallisonjuniodias@gmail.com>
2025-04-05 00:05:44 -0300

    Condicionais

---
+++ Estruturas de Repetição/src/ExemploFor.java
@@ -0,0 +1,7 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+        for (double carnerinhos = 1.0; carnerinhos <=20.0;  carnerinhos ++){
+            System.out.println("Contando Carnerinhos: " + carnerinhos);
+        }
+    }
+}
