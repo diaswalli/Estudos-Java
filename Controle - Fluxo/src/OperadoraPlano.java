8f92ff94f6141afb8fcad1f9e615f4cb5b8bcfa4
diaswalli <wallisonjuniodias@gmail.com>
2025-04-04 22:32:36 -0300

    Condicionais

---
+++ Controle - Fluxo/src/OperadoraPlano.java
@@ -0,0 +1,18 @@
+public class OperadoraPlano {
+    
+    public static void main(String[] args) {
+        String plano = "M";
+
+        switch (plano){
+            case "T": {
+                System.out.println("5GB Youtube");
+            }
+            case "M": {
+                System.out.println("Whats e Instagram Gratis");
+            }
+            case "B": {
+                System.out.println("100 minutos de ligação");
+            }
+        }
+    }
+}
---
+++ Controle - Fluxo/src/PlanOperadora.java
@@ -0,0 +1,17 @@
+public class PlanOperadora {
+    
+    public static void main(String[] args) {
+        String plano = "M";
+
+        if(plano == "B") {
+           System.out.println("100 minutos de ligação"); 
+        }else if(plano == "M") {
+            System.out.println("100 minutos de ligação");
+            System.out.println("Whats e Instagram gratis");
+        }else if(plano == "T") {
+            System.out.println("100 minutos de ligação");
+            System.out.println("Whats e Instagram gratis");
+            System.out.println("5GB Youtube");
+        }
+    }
+}
