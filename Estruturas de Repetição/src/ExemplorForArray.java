31cd318b5c34f18ab4f78f147d6968091cf1a5b1
diaswalli <wallisonjuniodias@gmail.com>
2025-04-06 19:24:50 -0300

    Condicionais

---
+++ Estruturas de Repetição/src/ExemplorForArray.java
@@ -0,0 +1,10 @@
+public class ExemplorForArray {
+    
+    public static void main(String[] args) {
+        String alunos[] = { "FELIPE", "JONAS", "MARCOS"};
+
+    for(int x=0; x < alunos.length; x++) {
+        System.out.print("O aluno no indice x=" + x + " é " + alunos[ x ]);
+    }
+    }
+}
